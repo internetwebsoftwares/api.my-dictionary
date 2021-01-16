614bfdf712c958c67e493b3c07dc92c925e1248d
Ata Shaikh <atasteven49@gmail.com>
2021-01-16 15:44:24 +0530

    First commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get("/version", (req, res) => {
   res.json({
